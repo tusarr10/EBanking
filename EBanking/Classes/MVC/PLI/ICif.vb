802e2ba1f8bcc1324364a2e3223f6250476f0734
tusarr10 <37357117+tusarr10@users.noreply.github.com>
2021-08-01 21:03:04 +0530

    test

--- EBanking/Classes/MVC/PLI/ICif.vb
+++ EBanking/Classes/MVC/PLI/ICif.vb
@@ -1,5 +1,5 @@
 ﻿Public Interface ICif
-
+    'interface
     Function GetAll() As IList(Of ClassCif)
     Function FindById(cif As String) As ClassCif
     Function AddCif(custmor As ClassCif) As Boolean
